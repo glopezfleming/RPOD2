7a0a60c56ee1c64c5ed68b3ab4d1b0c985d6b993
glopezfleming <gustavo.lopez-fleming.jr.26@dartmouth.edu>
2024-05-02 13:42:53 -0400

    Cleared App.js and assigned browser routes

--- nasa-rpod/src/App.js
+++ nasa-rpod/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./components/Home";
+import NasaPhoto from "./components/NasaPhoto";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route component = {Home} path = "/" exact/>
+        <Route component = {NasaPhoto} path = "/nasaphoto"/>
+      </div>
+    
+    </BrowserRouter>
   );
 }
 
